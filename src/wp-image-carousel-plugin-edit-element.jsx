c64acef7e65230dcaa70b914c1bfb8fbaaa2da9b
DylanHarrisonGithub <dillysnaps@gmail.com>
2019-09-23 01:56:41 -0500

    block save jsx implemented

--- src/wp-image-carousel-plugin-edit-element.jsx
+++ src/wp-image-carousel-plugin-edit-element.jsx
@@ -1,24 +1,33 @@
 <div>
   {(props.attributes.imageUrls && props.attributes.imageUrls.length) && 
-    <div class="wp-img-carousel-container">
-      <img class={
-          "wp-img-carousel-img" + (props.attributes._imgPairity ? " wp-img-carousel-img-transparent" : "")
-        } src={props.attributes.imageUrls[props.attributes.img1Index]} />
-      <img class={
-          "wp-img-carousel-img" + (!(props.attributes._imgPairity) ? " wp-img-carousel-img-transparent" : "")
-        } src={props.attributes.imageUrls[props.attributes.img2Index]} />
-      <span id="wp-img-carousel-left-arrow" onClick={stepBack}>
-        <span class="dashicons dashicons-arrow-left-alt2"></span>
-      </span>
-      <span id="wp-img-carousel-right-arrow" onClick={stepForward}>
-        <span class="dashicons dashicons-arrow-right-alt2"></span>
-      </span>
-      <span id="wp-img-carousel-dots-container">
-        {props.attributes.imageUrls.map((obj, i) => {
-          return (<div class={"wp-img-carousel-dot" + (((i==props.attributes.img1Index && !props.attributes._imgPairity) || (i==props.attributes.img2Index && props.attributes._imgPairity))? " wp-img-carousel-dot-active" : "")} onClick={() => setIndex(i)}></div>);
-        })}
-      </span>
+    <div>
+      <div class="wp-img-carousel-container" style={{
+        "background-color": props.attributes.backgroundColor,
+        "padding-top": props.attributes.aspectRatio
+      }}>
+        <img class={
+            "wp-img-carousel-img" + (props.attributes._imgPairity ? " wp-img-carousel-img-transparent" : "")
+          } src={props.attributes.imageUrls[props.attributes.img1Index]} />
+        <img class={
+            "wp-img-carousel-img" + (!(props.attributes._imgPairity) ? " wp-img-carousel-img-transparent" : "")
+          } src={props.attributes.imageUrls[props.attributes.img2Index]} />
+        <span id="wp-img-carousel-left-arrow" onClick={stepBack}>
+          <span class="dashicons dashicons-arrow-left-alt2"></span>
+        </span>
+        <span id="wp-img-carousel-right-arrow" onClick={stepForward}>
+          <span class="dashicons dashicons-arrow-right-alt2"></span>
+        </span>
+        <span id="wp-img-carousel-dots-container">
+          {props.attributes.imageUrls.map((obj, i) => {
+            return (<div class={"wp-img-carousel-dot" + (((i==props.attributes.img1Index && !props.attributes._imgPairity) || (i==props.attributes.img2Index && props.attributes._imgPairity))? " wp-img-carousel-dot-active" : "")} onClick={() => setIndex(i)}></div>);
+          })}
+        </span>
+      </div>
+      <div id="wp-img-carousel-control-panel">
+        <h3>Choose a background color</h3>
+        <wp.components.ColorPicker onChangeComplete={updateBackgroundColor} color={props.attributes.backgroundColor}/> 
+      </div>
     </div>
-  } 
-  <button class="button aligncenter" onClick={openMediaModal}>Select Images</button>
+  }
+  <button class="wp-img-carousel-button" onClick={openMediaModal}>Select Images</button>
 </div>
---
+++ src/wp-image-carousel-plugin-save.jsx
@@ -0,0 +1,26 @@
+<div>
+  {(props.attributes.imageUrls && props.attributes.imageUrls.length) && 
+    <div class="wp-img-carousel-container" style={{
+      "background-color": props.attributes.backgroundColor,
+      "padding-top": props.attributes.aspectRatio
+    }}>
+      <img class={
+          "wp-img-carousel-img" + (props.attributes._imgPairity ? " wp-img-carousel-img-transparent" : "")
+        } src={props.attributes.imageUrls[props.attributes.img1Index]} />
+      <img class={
+          "wp-img-carousel-img" + (!(props.attributes._imgPairity) ? " wp-img-carousel-img-transparent" : "")
+        } src={props.attributes.imageUrls[props.attributes.img2Index]} />
+      <span id="wp-img-carousel-left-arrow" onClick={stepBack}>
+        <span class="dashicons dashicons-arrow-left-alt2"></span>
+      </span>
+      <span id="wp-img-carousel-right-arrow" onClick={stepForward}>
+        <span class="dashicons dashicons-arrow-right-alt2"></span>
+      </span>
+      <span id="wp-img-carousel-dots-container">
+        {props.attributes.imageUrls.map((obj, i) => {
+          return (<div class={"wp-img-carousel-dot" + (((i==props.attributes.img1Index && !props.attributes._imgPairity) || (i==props.attributes.img2Index && props.attributes._imgPairity))? " wp-img-carousel-dot-active" : "")} onClick={() => setIndex(i)}></div>);
+        })}
+      </span>
+    </div>
+  }
+</div>
